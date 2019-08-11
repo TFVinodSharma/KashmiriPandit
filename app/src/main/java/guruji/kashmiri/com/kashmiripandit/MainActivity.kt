baf4d88f96a71ce5c05c6fc2a43b82477a31523d
Dell <vinodsharma9643@gmail.com>
2019-08-11 17:51:31 +0530

    my first kotlin program via recylcerview and  cardview

---
+++ app/src/main/java/guruji/kashmiri/com/kashmiripandit/MainActivity.kt
@@ -0,0 +1,29 @@
+package guruji.kashmiri.com.kashmiripandit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var recyclerView=findViewById<RecyclerView>(R.id.recycler)
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+//        Create an arraylist
+        val dataList = ArrayList<Model>()
+        dataList.add(Model("Phone", 1))
+        dataList.add(Model("Watch", 2))
+        dataList.add(Model("Note", 3))
+        dataList.add(Model("Pin", 4))
+//        pass the values to RvAdapter
+        val rvAdapter = RvAdapter(dataList)
+//        set the recyclerView to the adapter
+        recyclerView.adapter = rvAdapter;
+    }
+}
---
+++ app/src/main/java/guruji/kashmiri/com/kashmiripandit/Model.kt
@@ -0,0 +1,3 @@
+package guruji.kashmiri.com.kashmiripandit
+
+data class Model(val name: String = "", val count: Int = 0)
